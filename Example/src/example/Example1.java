f97b0fa1345df1a6926801f53579b5af43ed4277
yura jung <jungyr24@hanmail.net>
2019-12-23 16:14:56 +0900

    add one line

--- Example/src/example/Example1.java
+++ Example/src/example/Example1.java
@@ -11,8 +11,6 @@ public class Example1 {
         try {
             int input = in.read();
             System.out.println(input);
-	
-	System.out.println("ddd");
             out.write(input);
             out.close();
         } catch (IOException e) {
